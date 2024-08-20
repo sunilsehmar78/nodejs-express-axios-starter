641c3b2f8fad92566609fb3473f4cc5f0c528de1
AZZohaib <zohaib.ali@kainos.com>
2024-08-20 08:24:53 +0100

    fixed css, imgs, and added search by name

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,8 @@ import express from "express";
 import nunjucks from "nunjucks";
 import bodyParser from "body-parser";
 import session from "express-session";
-
-import { getAllDatabases } from "./controllers/TestController";
 import { getMeal } from "./controllers/SingleMealController";
+import { getNameResult } from "./controllers/SeachByNameController";
 
 const app = express();
 
@@ -18,6 +17,11 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
+// Specifing folder from where to serve static files (gets rid of MIME type CSS issue)
+app.use(express.static("views"));
+// Specifying folder from where to fetch images
+app.use('/assets', express.static('./src/assets')); 
+
 app.use(session({ secret: 'SUPER_SECRET', cookie: { maxAge: 28800000 }}));
 
 declare module "express-session" {
@@ -30,6 +34,10 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.get('/', getAllDatabases);
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.render('index.html'
+  );
+});
 
 app.get('/singleMeal', getMeal);
+app.post('/search/', getNameResult);
